44e754bb895085e50a7e90af126e70802d4251ef
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-10 15:02:49 +0500

    Takrorlash

---
+++ Takrorolash/Takrorolash/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Takrorolash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
