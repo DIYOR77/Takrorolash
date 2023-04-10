0244704226c0f18bf4e3b616482265b5f8856855
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-10 15:18:13 +0500

    Raqamlar yigindisini topish

--- Takrorolash/Takrorolash/Program.cs
+++ Takrorolash/Takrorolash/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write(" Enter number: ");
+            int son= int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Raqamlari yigindisi: "+Xonalaryigindisi(son));
+
+        }
+        public static int Xonalaryigindisi(int son)
+        {
+            int sonyigindisi = 0;
+            while (true)
+            {
+                if (son > 0)
+                {
+                    sonyigindisi += son % 10;
+                    son = son / 10;
+                }
+                else
+                {
+                    return sonyigindisi; 
+                }
+
+            } 
         }
     }
 }
